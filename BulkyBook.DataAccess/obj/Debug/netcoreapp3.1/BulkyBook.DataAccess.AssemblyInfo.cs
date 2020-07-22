e56af272b741fb66c4cbe588376f29eac4e52d90
Ayush Gajjar <amgajjar2099@gmail.com>
2020-07-22 22:55:53 +0530

    Initial Commit

---
+++ BulkyBook.DataAccess/obj/Debug/netcoreapp3.1/BulkyBook.DataAccess.AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.DataAccess")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.DataAccess")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.DataAccess")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
+
+
